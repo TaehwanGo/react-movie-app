6d5ca5deecf226ab144528b798a273b4752aa64c
TaehwanGo <gth1123@naver.com>
2021-03-05 22:04:04 +0900

    comment

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
+    // ?api_key=10923b261ba94d897ac6b81148314a3f&language=en-US
     api_key: '10923b261ba94d897ac6b81148314a3f',
     language: 'en-US',
   },
@@ -21,7 +22,7 @@ export const moviesApi = {
   search: term =>
     api.get('search/movie', {
       params: {
-        query: encodeURIComponent(term),
+        query: encodeURIComponent(term), // UTF-8로 특수문자 같은 것을 인코딩함(공백은 %20같은 것으로)
       },
     }),
 };
