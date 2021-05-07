050a655ae95725316fe96c108920f69cb5feff9a
TaehwanGo <gth1123@naver.com>
2021-05-08 00:36:41 +0900

    folder name

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Section from '../../Components/Section';
-import Loader from '../../Components/Loader';
-import Message from '../../Components/Message';
-import Poster from '../../Components/Poster';
+import Section from '../../components/Section';
+import Loader from '../../components/Loader';
+import Message from '../../components/Message';
+import Poster from '../../components/Poster';
 
 const Container = styled.div`
   padding: 20px;
